c57a7352a485f37ff4d4927d6bb1d75663486cb8
Aleksei Naida <naida@ledas.com>
2024-10-13 13:53:35 +0700

    first lab

---
+++ navigation/app/src/main/java/com/example/navigation/MainActivity.kt
@@ -0,0 +1,33 @@
+package com.example.navigation
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        findViewById<Button>(R.id.next_button_A).setOnClickListener(View.OnClickListener {
+            val i = Intent(
+                applicationContext,
+                SecondActivity::class.java
+            )
+            startActivity(i)
+        })
+
+    }
+}
