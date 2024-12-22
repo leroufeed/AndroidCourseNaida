76c113099c7ff91ef5dbb6f3e5d96210f23912b9
Aleksei Naida <naida@ledas.com>
2024-12-22 16:43:25 +0700

    view update

--- MusicPlayer/app/build.gradle.kts
+++ MusicPlayer/app/build.gradle.kts
@@ -62,6 +62,7 @@ dependencies {
     implementation(libs.androidx.appcompat)
     implementation(libs.androidx.recyclerview)
     implementation(libs.androidx.constraintlayout)
+    implementation(libs.material)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
